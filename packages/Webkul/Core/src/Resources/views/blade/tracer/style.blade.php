9c98c476c54cfa0d34063df5c77f578427b00c46
webase-soluations <hellowebase@gmail.com>
2021-03-14 16:17:28 +0600

    change single product view section

--- packages/Webkul/Core/src/Resources/views/blade/tracer/style.blade.php
+++ packages/Webkul/Core/src/Resources/views/blade/tracer/style.blade.php
@@ -1,14 +1,14 @@
 <style>
-    .path-hint {
+     {
         border: solid 1px transparent;
         padding: 1px;
     }
 
-    .path-hint:hover {
+    :hover {
         border: 1px solid red;
     }
 
-    .path-hint-tooltip {
+    -tooltip {
         padding: 0px 10px;
         position: absolute;
         background: #000000;
@@ -17,14 +17,14 @@
         font-size: 10px;
     }
 
-    .path-hint-tooltip h4 {
+    -tooltip h4 {
         margin-top: 5px;
         margin-bottom: 3px;
         color: #fff;
         font-size: 12px;
     }
 
-    .path-hint-tooltip ul li {
+    -tooltip ul li {
         margin-bottom: 3px;
     }
 
@@ -36,7 +36,7 @@
 <script>
     window.addEventListener("load", function(event) {
         $('.testing').each(function(index) {
-            if ($(this).siblings(':not(.path-hint)').length == 1
+            if ($(this).siblings(':not()').length == 1
                 && $(this).next().prop("tagName") != 'INPUT'
                 && $(this).next().prop("tagName") != 'TEXTAREA'
                 && $(this).next().prop("tagName") != 'SELECT'
@@ -45,8 +45,8 @@
 
                 $(this).next().attr({
                     'data-toggle': 'tooltip',
-                    'data-title': $(this).parent('.path-hint').attr('data-title'),
-                    'data-id': $(this).parent('.path-hint').attr('data-id')
+                    'data-title': $(this).parent('').attr('data-title'),
+                    'data-id': $(this).parent('').attr('data-id')
                 });
 
                 $(this).unwrap();
@@ -55,19 +55,19 @@
             $(this).remove();
         });
 
-        $('.path-hint').on('mouseover', function(e) {
+        $('').on('mouseover', function(e) {
             e.stopPropagation();
 
             var currentElement = $(e.currentTarget);
 
-            var tooltipContent = '<h4>{{ __("core::app.path-hint.template") }}</h4>' + currentElement.attr('data-title');
+            var tooltipContent = '<h4>{{ __("core::app.template") }}</h4>' + currentElement.attr('data-title');
 
-            if ($(this).parents('.path-hint').length) {
-                tooltipContent += '<h4>{{ __("core::app.path-hint.parents") }}</h4>';
+            if ($(this).parents('').length) {
+                tooltipContent += '<h4>{{ __("core::app.parents") }}</h4>';
 
                 tooltipContent += '<ul>';
 
-                $(this).parents('.path-hint').each(function(index) {
+                $(this).parents('').each(function(index) {
                     tooltipContent += '<li>' + $(this).attr('data-title')  + '</li>';
                 });
 
@@ -78,7 +78,7 @@
 
             var elementWidth = currentElement.outerWidth()
 
-            var tooltipWidth = $('.path-hint-tooltip').outerWidth()
+            var tooltipWidth = $('-tooltip').outerWidth()
 
             var leftOffset = currentElement.offset().left;
 
@@ -100,9 +100,9 @@
                 left = 10;
             }
 
-            $('.path-hint-tooltip').css('left', left - minus)
+            $('-tooltip').css('left', left - minus)
 
-            $('.path-hint-tooltip').css('top', currentElement.offset().top + 20)
+            $('-tooltip').css('top', currentElement.offset().top + 20)
         })
 
         $('[data-toggle="tooltip"]').on('mouseout', function(e) {
